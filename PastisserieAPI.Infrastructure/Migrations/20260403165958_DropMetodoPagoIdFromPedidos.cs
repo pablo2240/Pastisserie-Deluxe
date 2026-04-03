8ab8a582cb7370a1e79cec9d92ada9c2b37510cc
Pablo <reyestorrespablo22@gmail.com>
2026-04-03 14:56:41 -0500

    chore: add DropMetodoPagoId migration and update gitignore for production files

---
+++ PastisserieAPI.Infrastructure/Migrations/20260403165958_DropMetodoPagoIdFromPedidos.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PastisserieAPI.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class DropMetodoPagoIdFromPedidos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                IF EXISTS (SELECT 1 FROM sys.columns WHERE object_id = OBJECT_ID('Pedidos') AND name = 'MetodoPagoId')
+                BEGIN
+                    ALTER TABLE [Pedidos] DROP COLUMN [MetodoPagoId];
+                END
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>("MetodoPagoId", "Pedidos", nullable: true);
+        }
+    }
+}
